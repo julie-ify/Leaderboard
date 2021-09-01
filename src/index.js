74e63de71447dd4adeaa5aa8808a20bd6a8f8ac2
Juliana Ifionu <julieifionu@gmail.com>
2021-09-01 03:57:07 +0100

    Add error msg when user submit empty value

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import displayScore from './displayScore.js';
 const addButton = document.querySelector('.add');
 const refreshButton = document.querySelector('.refresh');
 const listContainer = document.querySelector('.lists');
+const paragraph = document.querySelector('.paragraph');
+
 const api = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
 
 window.addEventListener('DOMContentLoaded', async () => {
@@ -18,14 +20,22 @@ window.addEventListener('DOMContentLoaded', async () => {
     e.preventDefault();
     const userName = document.querySelector('#user_name');
     const userScore = document.querySelector('#user_score');
-    if(userName.value === '' || userScore.value === '') {
-      return;
+    if (userName.value === '' || userScore.value === '') {
+      const errorMsg = document.createElement('p');
+      errorMsg.textContent = 'Please add your name and score';
+      errorMsg.style.display = 'block';
+      errorMsg.style.backgroundColor = 'red';
+      errorMsg.style.color = '#fff';
+      errorMsg.style.margin = '1rem auto 0';
+      errorMsg.style.width = '400px';
+      errorMsg.style.padding = '10px';
+      paragraph.appendChild(errorMsg);
     } else {
+      paragraph.innerHTML = '';
       await addScore(userName.value, userScore.value, `${api}${gameId}/scores/`);
       userName.value = '';
       userScore.value = '';
     }
-    
   });
 
   refreshButton.addEventListener('click', async (e) => {
