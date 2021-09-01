1ca746142dffb28fee1bec4deff40f47e681e339
Juliana Ifionu <julieifionu@gmail.com>
2021-09-01 01:54:40 +0100

    Create name for my game

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+import getData from './getData.js';
+import createGame from './createGame.js'
+
+window.addEventListener('DOMContentLoaded', async () => {
+  const {result} = await createGame()
+})
